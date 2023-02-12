f00a2e40821dd17b28aa8054d5d380831b685ee4
Leandro Basla <basla.leandro@pwc.com>
2023-02-12 05:12:36 -0300

    organize setup

--- 0_Setup.py
+++ 0_Setup.py
@@ -1,12 +1,18 @@
 import streamlit as st
 import pandas as pd
+from alearn.setup_functions import setup_labels
+st.set_page_config(layout="centered")
+
+
+COL_OPTIONS = ['TEXT', 'NUM', 'CAT']
+MODEL_TYPES = ['Binary', 'Multiclass', 'Multilabel', 'NER']
 
 
 def load_dataset():
     if 'dataset' in st.session_state:
         st.session_state['df'] = pd.read_csv(st.session_state['dataset'])
         st.session_state['col_status'] = {col: False for col in st.session_state['df']}
-        st.session_state['col_type'] = {col: None for col in st.session_state['df']}
+        st.session_state['col_type'] = {col: COL_OPTIONS[0] for col in st.session_state['df']}
 
 
 def toggle(col):
@@ -16,19 +22,15 @@ def toggle(col):
         st.session_state['col_status'][col] = True
 
 
-def add_label():
-    if st.session_state['new_label'] not in st.session_state['labels']:
-        st.session_state['labels'].append(st.session_state['new_label'])
+def change_selected_type(col):
+    st.session_state['col_type'][col] = st.session_state[f"selected_type_{col}"]
 
 
-def delete_label(label):
-    st.session_state['labels'].remove(label)
-
+def set_model_type():
+    st.session_state['model_type'] = st.session_state['selected_model_type']
 
 st.title("Active Learn")
 
-COL_OPTIONS = ['TEXT', 'NUM', 'CAT']
-MODEL_TYPES = ['Binary', 'Multiclass', 'Multilabel', 'NER']
 
 st.file_uploader("Upload a csv file", key='dataset', on_change=load_dataset)
 
@@ -51,8 +53,8 @@ if 'df' in st.session_state:
                 col_formatted = f'<p style="font-size: 24px;">{col}</p>'
                 st.markdown(col_formatted, unsafe_allow_html=True)
             with col2:
-                st.selectbox('', COL_OPTIONS, key=f"type_{str(col)}",
-                             label_visibility='collapsed')
+                st.selectbox('', COL_OPTIONS, key=f"selected_type_{str(col)}",
+                             label_visibility='collapsed', on_change=change_selected_type, args=(col,), index=0)
             with col3:
                 if st.session_state['col_status'][col]:
                     st.button('ENABLE', key=f"enable_{str(col)}", on_click=toggle, args=(col,), type='primary')
@@ -62,39 +64,8 @@ if 'df' in st.session_state:
     if len([1 for c in st.session_state['col_status'] if st.session_state['col_status'][c]]) == 0:
         st.error('Must enable at least one column')
     else:
+        if 'model_type' not in st.session_state:
+            st.session_state['model_type'] = MODEL_TYPES[0]
         st.header('Select model type')
-        st.radio('Model type', options=MODEL_TYPES, key='model_type')
-
-        if st.session_state['model_type'] != 'Binary':
-            if 'labels' not in st.session_state:
-                st.session_state['labels'] = []
-            st.markdown("""---""")
-            st.title("Create training labels")
-            col1, col2 = st.columns((8, 2))
-            with col1:
-                st.text('Label name')
-            with col2:
-                st.text('Action')
-
-            for label in st.session_state['labels']:
-                col1, col2 = st.columns((8, 2))
-                with col1:
-                    st.text(label)
-                with col2:
-                    st.button("🗑", key=f'delete_{label}', on_click=delete_label, args=(label, ))
-
-            col1, col2 = st.columns((8, 2))
-            with col1:
-                st.text_input('', label_visibility='collapsed', key='new_label', on_change=add_label)
-            with col2:
-                st.button('Add', on_click=add_label)
-
-            if len(st.session_state['labels']) > 1 or \
-                    (st.session_state['model_type'] == 'NER' and len(st.session_state['labels']) > 0):
-                st.success('You can move to the next page to start training!')
-            else:
-                st.warning('Create labels')
-        else:
-            st.success('You can move to the next page to start training!')
-
-# st.session_state
+        st.radio('Model type', options=MODEL_TYPES, key='selected_model_type', on_change=set_model_type, index=0)
+        setup_labels()
---
+++ alearn/setup_functions.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+
+def add_label():
+    if st.session_state['new_label'] not in st.session_state['labels']:
+        st.session_state['labels'].append(st.session_state['new_label'])
+
+
+def delete_label(label):
+    st.session_state['labels'].remove(label)
+
+
+def setup_labels():
+
+    if 'labels' not in st.session_state:
+        st.session_state['labels'] = []
+    st.markdown("""---""")
+    st.title("Create training labels")
+    col1, col2 = st.columns((8, 2))
+    with col1:
+        st.text('Label name')
+    with col2:
+        st.text('Action')
+
+    for label in st.session_state['labels']:
+        col1, col2 = st.columns((8, 2))
+        with col1:
+            st.text(label)
+        with col2:
+            st.button("🗑", key=f'delete_{label}', on_click=delete_label, args=(label,))
+
+    col1, col2 = st.columns((8, 2))
+    with col1:
+        st.text_input('', label_visibility='collapsed', key='new_label', on_change=add_label)
+    with col2:
+        st.button('Add', on_click=add_label)
+
+    if st.session_state['model_type'] == 'Binary' and len(st.session_state['labels']) != 2:
+        st.warning('Binary requires 2 labels')
+    elif st.session_state['model_type'] in ['Multiclass', 'Multilabel'] and len(st.session_state['labels']) <2:
+        st.warning(f"{st.session_state['model_type']} requires at least 2 labels")
+    elif len(st.session_state['labels']) > 1 or \
+            (st.session_state['model_type'] == 'NER' and len(st.session_state['labels']) > 0):
+        st.success('You can move to the next page to start training!')
+    else:
+        st.warning('Create labels')
