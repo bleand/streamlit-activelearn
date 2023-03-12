ebbf6752b4e695eff9b188e2c90754360107de84
Leandro Basla <basla.leandro@pwc.com>
2023-03-12 03:40:36 -0300

    controller builder uses MultiController

--- alearn/controller_builder.py
+++ alearn/controller_builder.py
@@ -1,6 +1,7 @@
 from .nlp_controller import NLPController
-
+from .multi_controller import MultiController
 
 def controller_from_settings(session):
-    if 'TEXT' == 'TEXT': # Should be replaced with the dtype validation
-        return NLPController(session)
+    return MultiController(session)
+    #if 'TEXT' != 'TEXT': # Should be replaced with the dtype validation
+    #    return NLPController(session)
