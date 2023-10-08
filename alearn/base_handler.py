34f7ef1540b8682f50b6627f6424ed4331e461bc
Leandro Basla <basla.leandro@pwc.com>
2023-10-08 02:40:40 -0300

    Updated base handler

--- alearn/base_handler.py
+++ alearn/base_handler.py
@@ -3,6 +3,10 @@ class BaseHandler:
     def __init__(self, dtype, col_name):
         self.dtype = dtype
         self.col_name = col_name
+        self.seeds = []
+        self.vectorizer = None
+        self.seeds_vectors = None
+        self.pool_vectors = None
 
     def new_seed_row(self):
         raise NotImplementedError
@@ -11,4 +15,22 @@ class BaseHandler:
         raise NotImplementedError
 
     def seed_exists(self):
+        raise NotImplementedError
+
+    def init_vectorizer(self, df):
+        raise NotImplementedError
+
+    def vectorize_seeds(self):
+        raise NotImplementedError
+
+    def vectorize_pool(self, df):
+        raise NotImplementedError
+
+    def vectorize(self, df):
+        raise NotImplementedError
+
+    def get_data(self, df):
+        raise NotImplementedError
+
+    def display_sample(self, df, ix):
         raise NotImplementedError
