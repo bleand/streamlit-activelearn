288731c48c495e30dc67605b64d23ded5fb99b85
Leandro Basla <basla.leandro@pwc.com>
2023-10-08 02:43:59 -0300

    Improved handler builder

--- alearn/handler_builder.py
+++ alearn/handler_builder.py
@@ -2,5 +2,6 @@ from .text_handler import TextHandler
 
 
 def handler_from_settings(dtype, col_name):
-    return TextHandler(dtype, col_name)
+    if dtype == 'TEXT':
+        return TextHandler(dtype, col_name)
 
