febf5e37fafca19d869e3d837ad1b68740f30743
Leandro Basla <basla.leandro@pwc.com>
2023-10-08 02:40:59 -0300

    Implemented text handler

--- alearn/text_handler.py
+++ alearn/text_handler.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from .base_handler import BaseHandler
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 class TextHandler(BaseHandler):
 
     def __init__(self, *args, **kwargs):
-        self.seeds = []
         super(TextHandler, self).__init__(*args, **kwargs)
 
     def new_seed_row(self):
@@ -20,5 +20,34 @@ class TextHandler(BaseHandler):
         self.seeds.append(st.session_state[f'{self.col_name}_new_seed'])
         st.session_state[f'{self.col_name}_new_seed'] = ''
 
+    def delete_seed(self, seed_ix):
+        del self.seeds[seed_ix]
+
     def seed_exists(self):
         return st.session_state[f'{self.col_name}_new_seed'] in self.seeds
+
+    def init_vectorizer(self, df):
+        self.vectorizer = TfidfVectorizer(max_features=1000)
+
+        self.vectorizer.fit(self.get_data(df))
+
+    def vectorize_seeds(self):
+        seed_vectors = []
+        for seed in self.seeds:
+            seed_vectors.append(self.vectorizer.transform([seed]).todense())
+        self.seeds_vectors = seed_vectors
+
+    def vectorize_pool(self, df):
+        self.pool_vectors = self.vectorizer.transform(self.get_data(df)).todense()
+
+    def vectorize(self, df):
+        return self.vectorizer.transform(self.get_data(df)).toarray()
+
+    def get_data(self, df):
+        # return df[df[self.col_name].fillna(' ').astype(str).str.strip().map(len) != 0][self.col_name].tolist()
+        return df[self.col_name].fillna(' ').tolist()
+
+    def display_sample(self, df, ix):
+        sample = df.iloc[ix][self.col_name]
+        st.subheader(self.col_name)
+        st.markdown(sample)
